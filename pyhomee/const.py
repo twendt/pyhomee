b34b71ed4465ea775d9c6f39bdd7ca9ee8e86ef9
Timo Wendt <timo@tjwendt.de>
2017-11-05 21:34:24 +0100

    Add initial constants

---
+++ pyhomee/const.py
@@ -0,0 +1,15 @@
+SWITCH = 1
+BRIGHTNESS = 2
+SATURATION = 206
+BATTERY = 8
+ENERGY = 4
+POWER = 3
+
+ATTR_TYPES = {
+  SWITCH: "switch",
+  BRIGHTNESS: "brightness",
+  SATURATION: "saturation",
+  BATTERY: "battery",
+  ENERGY: "energy",
+  POWER: "power"
+}
