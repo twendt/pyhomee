140ef8151cc3695888227ca1a16ce04097a58e7c
Timo Wendt <timo@tjwendt.de>
2017-11-07 22:59:35 +0100

    Bump version to 0.0.2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='pyhomee',
-      version='0.0.1',
+      version='0.0.2',
       description='Access Homee Websocket API',
       url='http://github.com/twendt/pyhomee',
       author='Timo Wendt',
